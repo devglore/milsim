aab98cb0ac0717d607d3f949f397d905dde15afd
Marcus <glorebard@gmail.com>
2015-10-21 19:47:24 +0000

    Added loginbutton for steam

--- include/steamauth/logout.php
+++ include/steamauth/logout.php
@@ -1,6 +1,6 @@
 <?php
 include("settings.php");
-header("Location: ".$steamauth['logoutpage']);
+header("Location: ../../".$steamauth['logoutpage']);
 session_start();
 unset($_SESSION['steamid']);
 unset($_SESSION['steam_uptodate']);
