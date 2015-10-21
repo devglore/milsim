0b332094d16976a4310f9077bf2954511278d909
Marcus <glorebard@gmail.com>
2015-10-21 19:45:57 +0000

    Added loginbutton for steam

--- include/steamauth/settings.php
+++ include/steamauth/settings.php
@@ -2,8 +2,8 @@
 $steamauth['apikey'] = "8F74FC05DA724DC6F53FC33907A9F167"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "localhost/MilSim/"; // The main URL of your website displayed in the login page
 $steamauth['buttonstyle'] = "small"; // Style of the login button [small|large_no|large]
-$steamauth['logoutpage'] = "../../index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = "../../index.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "../index.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!</div>");}
