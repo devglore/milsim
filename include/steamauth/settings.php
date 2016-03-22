f37a2e47ff3517add048c0334b02e45dcc29e89c
Marcus <marcusalven@gmail.com>
2016-03-22 08:26:42 +0100

    Update settings.php

--- include/steamauth/settings.php
+++ include/steamauth/settings.php
@@ -1,5 +1,5 @@
 <?php
-$steamauth['apikey'] = "8F74FC05DA724DC6F53FC33907A9F167"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
+$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "localhost/MilSim/"; // The main URL of your website displayed in the login page
 $steamauth['buttonstyle'] = "small"; // Style of the login button [small|large_no|large]
 $steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
